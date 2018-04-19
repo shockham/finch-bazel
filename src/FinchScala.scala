1754ebf3a75543d057c90120f339ff78d9921952
shockham <samuel.hockham@gmail.com>
2018-04-19 11:31:05 +0100

    Fix the app so it actually runs

--- src/FinchScala.scala
+++ src/FinchScala.scala
@@ -1,7 +1,8 @@
 import io.finch._
 import com.twitter.finagle.Http
+import com.twitter.util.Await
 
 object FinchScala extends App {
   val api: Endpoint[String] = get("hello") { Ok("Hello, World!") }
-  Http.server.serve(":8080", api.toServiceAs[Text.Plain])
+  Await.ready(Http.server.serve(":8080", api.toServiceAs[Text.Plain]))
 }
